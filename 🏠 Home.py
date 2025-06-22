d12d8efeb58719c1b8fa5f7a5c11fcef552f0ccc
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2025-06-22 17:52:35 +0700

    Update 🏠 Home.py

--- 🏠 Home.py
+++ 🏠 Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Dự đoán rời bỏ", layout="wide", initial_sidebar_state="expanded")
 
-st.title("👋 Chào mừng Anh Bùi Ngọc Tùng đến với ứng dụng Dự đoán khách bàng Rời bỏ Khách hàng")
+st.title("👋 Chào mừng Anh Bùi Ngọc Tùng đến với ứng dụng Dự đoán khách hàng Rời bỏ Ngân hàng")
 st.markdown("""
 Ứng dụng này giúp ngân hàng dự đoán **khách hàng có khả năng rời bỏ (churn)** dựa trên hành vi gần đây của họ như:
 - Số giao dịch,
